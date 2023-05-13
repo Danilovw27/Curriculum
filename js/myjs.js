6a4afcc0988cfa034d3cd546631020f2e44f4bf1
Danilovw27 <danilow333@gmail.com>
2023-05-12 21:36:15 -0500

    CV

--- js/myjs.js
+++ js/myjs.js
@@ -1,22 +1,20 @@
 let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
 let icon_hamburguer = true;
 
-/*######################### Investigar metodos y eventos de js[[[[[]]]]] ###########*/
-
-document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
-    if(icon_hamburguer){
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+    if (icon_hamburguer){
         document.querySelectorAll(".hamburguer")[0].style.color="#fff";
         icon_hamburguer = false;
-    }else{
+    }
+    else{
         document.querySelectorAll(".hamburguer")[0].style.color="#000";
-        icon_hamburguer = true;
+        icon_hamburguer= true;
     }
 
-    enlaces_header.classList.toogle("hamburgueropen")
-
-    document.querySelector(".enlaces-header")[0].addEventListener("click", function(){
-        enlaces_header.classList.toogle("hamburgueropen");
-        document.querySelectorAll(".hamburguer")[0].style.color="#000"
-    })
+    enlaces_header.classList.toggle("hamburgueropen");
 })
 
+document.querySelectorAll(".enlaces-header")[0].addEventListener("click", function(){
+    enlaces_header.classList.toggle("hamburgueropen");
+    document.querySelectorAll(".hamburguer")[0].style.color = "#000";
+})
